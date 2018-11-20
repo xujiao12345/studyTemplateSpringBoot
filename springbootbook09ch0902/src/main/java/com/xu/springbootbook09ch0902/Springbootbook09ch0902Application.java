7bc17fd80688bd4b2d270f6486975e36be94f619
zhangcheng <912649821@qq.com>
2018-11-20 19:08:23 +0800

    dd

---
+++ springbootbook09ch0902/src/main/java/com/xu/springbootbook09ch0902/Springbootbook09ch0902Application.java
@@ -0,0 +1,12 @@
+package com.xu.springbootbook09ch0902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//将csv的文件使用批处理的方式插入数据库
+@SpringBootApplication
+public class Springbootbook09ch0902Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootbook09ch0902Application.class, args);
+    }
+}
---
+++ springbootbook09ch0902/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table person(
+  id int not null primary key,
+  name varchar(20),
+  age int,
+  nation varchar(20),
+  address varchar(20)
+);
